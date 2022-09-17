19fd1d8a64131346c64876137197f9ba841636d5
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-17 15:27:41 -0500

    Write key and value to a db

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ fn main() {
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap(); // unwrap will crash the program if not found
     let value = args.next().expect("Argument <value> not provided"); // expect works like unwrap but it can show a custom msg
-    println!("The key is {} and the value is {}", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
 }
