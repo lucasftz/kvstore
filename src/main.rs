7c1ec14183e72b107b7d7b2c4404d8dea002db09
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-17 15:15:44 -0500

    Read args passed in

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    // get the arguments the program was called with
+    let mut args = std::env::args().skip(1);
+    let key = args.next().unwrap(); // unwrap will crash the program if not found
+    let value = args.next().unwrap();
+    println!("The key is {} and the value is {}", key, value);
 }
