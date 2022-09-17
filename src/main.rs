0587d8eb527b82e508455ce26f55f8839e0424b2
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-17 15:21:45 -0500

    Use expect instead of unwrap

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ fn main() {
     // get the arguments the program was called with
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap(); // unwrap will crash the program if not found
-    let value = args.next().unwrap();
+    let value = args.next().expect("Argument <value> not provided"); // expect works like unwrap but it can show a custom msg
     println!("The key is {} and the value is {}", key, value);
 }
