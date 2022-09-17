ee64d22e264d1a37c5df65818650e413154b2a50
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-17 16:38:31 -0500

    String interpolation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap(); // unwrap will crash the program if not found
     let value = args.next().expect("Argument <value> not provided"); // expect works like unwrap but it can show a custom msg
-    let contents = format!("{}\t{}\n", key, value);
+    let contents = format!("{key}\t{value}\n");
     std::fs::write("kv.db", contents).unwrap();
 
     let _database = Database::new().expect("Database::new() crashed");
