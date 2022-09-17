ac1e7178776851fbdf19f8a8558b72ec2c81dfe8
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-17 18:59:52 -0500

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 
 fn main() {
-    // get the arguments the program was called with
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap(); // unwrap will crash the program if not found
     let value = args.next().expect("Argument <value> not provided"); // expect works like unwrap but it can show a custom msg
@@ -17,23 +16,14 @@ struct Database {
 impl Database {
     fn new() -> Result<Database, std::io::Error> {
         // parse keys/values from kv.db
-        let contents = match std::fs::read_to_string("kv.db") {
-            Ok(c) => c, // bind contents to <c> and return it
-            Err(e) => {
-                return Err(e);
-            }
-        };
-        // a more concise way to write this would be the following
-        // let contents = std::fs::read_to_string("kv.db")?;
-
+        let contents = std::fs::read_to_string("kv.db")?;
         // populate map with keys/values
         let mut map: HashMap<String, String> = HashMap::new();
         for line in contents.lines() {
             let (key, value) = line.split_once('\t').expect("Corrupted database");
             map.insert(key.to_owned(), value.to_owned());
         }
-
-        Ok(Database { map: map })
+        Ok(Database { map })
     }
 
     fn insert(&mut self, key: String, value: String) {
@@ -44,8 +34,7 @@ impl Database {
         // writes keys and values from database to disk
         let mut contents = String::new();
         for (key, value) in &self.map {
-            let kvpair = format!("{}\t{}\n", key, value);
-            contents.push_str(&kvpair); // can also use + operator to concat strings
+            contents.push_str(&format!("{key}\t{value}\n"));
         }
         std::fs::write("kv.db", contents)
     }
