c5b549f18491565339b99980225f491d52051d47
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-18 01:58:28 -0500

    Change method name <throw_error> to <if_error>

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,33 @@ use std::collections::HashMap;
 fn main() {
     let mut database = Database::new().expect("Database::new() crashed");
     let mut args = std::env::args().skip(1);
-    let command = args.next().throw_error("argument <command> not provided");
+    let command = args.next().if_error("argument <command> not provided");
     match command.as_str() {
         "get" => {
-            let key = args.next().throw_error("argument <key> not provided");
-            println!("{}", database.get(key).throw_error("unknown key"))
+            let key = args.next().if_error("argument <key> not provided");
+            println!("{}", database.get(key).if_error("unknown key"))
         }
         "add" => {
-            let key = args.next().throw_error("argument <key> not provided");
-            let value = args.next().throw_error("argument <value> not provided");
+            let key = args.next().if_error("argument <key> not provided");
+            let value = args.next().if_error("argument <value> not provided");
             database.insert(key, value);
             database.flush().unwrap();
         }
         "remove" => {
-            let key = args.next().throw_error("argument <key> not provided");
-            database.remove(key).throw_error("unknown key");
+            let key = args.next().if_error("argument <key> not provided");
+            database.remove(key).if_error("unknown key");
             database.flush().unwrap();
         }
         "list" => {
-            let contents = database.keys_as_str().throw_error("empty database");
+            let contents = database.keys_as_str().if_error("empty database");
             println!("{contents}");
         }
         "help" => {
-            let contents = database
-                .get_help_txt()
-                .throw_error("help.txt does not exist");
+            let contents = database.get_help_txt().if_error("help.txt does not exist");
             println!("{contents}");
         }
         _ => {
-            database.unknown().throw_error("unknown command");
+            database.unknown().if_error("unknown command");
         }
     }
 }
@@ -53,7 +51,7 @@ impl Database {
                 map.insert(key.to_owned(), value.to_owned());
             }
         } else {
-            std::fs::File::create("kv.db").expect("Failed to create database");
+            std::fs::File::create("kv.db").expect("failed to create database");
         }
         Ok(Database { map })
     }
@@ -107,11 +105,11 @@ impl Database {
 }
 
 trait OptionWrapper<T> {
-    fn throw_error(self, msg: &str) -> T;
+    fn if_error(self, msg: &str) -> T;
 }
 
 impl<T> OptionWrapper<T> for Option<T> {
-    fn throw_error(self, msg: &str) -> T {
+    fn if_error(self, msg: &str) -> T {
         match self {
             Some(value) => value,
             None => {
